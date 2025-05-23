d3d716a9b78430dc8da6104e5f2132775887625d
irina <>
2025-05-23 23:24:22 +0500

    feat: register ItemsController and CreateItemService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { ToDosService } from './to-dos.service'
+import { ItemsController } from './ITEMS.controller'
+import { CreateItemService } from './create-item.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [ToDosService],
+  controllers: [AppController, ItemsController],
+  providers: [ToDosService, CreateItemService],
 })
 // eslint-disable-next-line prettier/prettier
 export class AppModule { }
