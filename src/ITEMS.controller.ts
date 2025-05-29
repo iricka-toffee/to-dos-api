34b1d3a108f37d06bf1de2a5128749838394f22d
irina <>
2025-05-29 13:17:06 +0500

    feat: add ItemController with create and get endpoints

--- src/ITEMS.controller.ts
+++ src/ITEMS.controller.ts
@@ -1,103 +1,29 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common'
-import { ApiOkResponse, ApiProperty, ApiQuery } from '@nestjs/swagger'
-import { ToDosService } from './to-dos.service'
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger'
+import { CreateItemService } from './create-item.service'
 
-class ToDo {
-  @ApiProperty()
-  id: number
-
-  @ApiProperty()
-  name: string
-}
-
-class NewTypeRequest {
+class CreateItemRequest {
   @ApiProperty()
   type: string
 }
 
-class NewItemResponse {
-  @ApiProperty({
-    type: Number,
-  })
+class CreateItemResponse {
+  @ApiProperty()
   newItemId: number
 }
 
-class ToDosResponse {
-  @ApiProperty({
-    isArray: true,
-    type: ToDo,
-  })
-  toDos: ToDo[]
-}
-
-class NewToDoRequest {
-  @ApiProperty()
-  name: string
-}
-
-class NewToDoResponse {
-  @ApiProperty({
-    type: Number,
-  })
-  newToDoId: number
-}
-
-class CompleteToDosRequest {
-  @ApiProperty({
-    isArray: true,
-    type: Number,
-  })
-  toDoIds: number[]
-}
-
-@Controller('items')
-export class ItemsController {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private readonly toDosService: ToDosService) { }
-
-  @ApiOkResponse({
-    description: 'ToDo items',
-    type: ToDosResponse,
-  })
-  @Get()
-  getToDos() {
-    return {
-      toDos: this.toDosService.getToDos(),
-    }
-  }
+@Controller('create-item')
+export class ItemController {
+  constructor(private readonly createItemService: CreateItemService) { }
 
   @Post()
-  @ApiOkResponse({
-    description: 'Created ToDo ID',
-    type: NewToDoResponse,
-  })
-  addToDo(@Body() { name }: NewToDoRequest) {
-    const { newToDoId } = this.toDosService.addToDo({
-      name,
-    })
-
-    return {
-      newToDoId,
-    }
+  @ApiOkResponse({ type: CreateItemResponse })
+  create(@Body() { type }: CreateItemRequest) {
+    return this.createItemService.createItem(type)
   }
 
-  @Post('complete')
-  completeToDos(@Body() { toDoIds = [] }: CompleteToDosRequest) {
-    toDoIds.forEach((toDoId) =>
-      this.toDosService.completeToDo({
-        id: toDoId,
-      }),
-    )
-  }
-
-  @Delete()
-  @ApiQuery({
-    name: 'toDoId',
-    type: Number,
-  })
-  deleteToDo(@Query('toDoId') toDoId: string) {
-    this.toDosService.completeToDo({
-      id: Number(toDoId),
-    })
+  @Get()
+  getAll() {
+    return this.createItemService.getItems()
   }
 }
